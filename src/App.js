b2fc9c13f6bd037d50d59bb097a15c9634d36f52
HariPrasad5724 <hariprasad572415@gmail.com>
2021-04-08 19:59:00 +0530

    With Build and Env 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import './App.css';
-import HeaderNav from './components/HeaderNav/HeaderNav.js';
-import SideBar from './components/SideBar/SideBar';
-import Medicines from './components/Home/Home.jsx';
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import "./App.css";
+import HeaderNav from "./components/HeaderNav/HeaderNav.js";
+import SideBar from "./components/SideBar/SideBar";
+import Medicines from "./components/Home/Home.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
       <Router>
-      <div>
-      <Switch>
-      <Route exact path="/">
-      <HeaderNav/>
-      <SideBar/>
-      </Route>
-      <Route exact path="/Home">
-      <HeaderNav/>
-      <SideBar/>
-      </Route>
-      <Route path="/ListMedicine">
-      <HeaderNav/>
-      <SideBar/>
-      <Medicines/>
-      </Route>
-      <Route path="/ListStore">
-      <HeaderNav/>
-      <SideBar/>
-      </Route>
-      </Switch>
-      </div>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <HeaderNav />
+              <SideBar />
+            </Route>
+            <Route exact path="/Home">
+              <HeaderNav />
+              <SideBar />
+            </Route>
+            <Route path="/ListMedicine">
+              <HeaderNav />
+              <SideBar />
+              <Medicines />
+            </Route>
+            <Route path="/ListStore">
+              <HeaderNav />
+              <SideBar />
+            </Route>
+          </Switch>
+        </div>
       </Router>
-     </div>
+    </div>
   );
 }
 
