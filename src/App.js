a2309030ea7a33dcef577ab3a9ffe0a9c6bc1805
Hari Prasad S A <55315604+HariPrasad5724@users.noreply.github.com>
2021-05-12 17:11:47 +0530

    Future enhancements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import HeaderNav from './components/HeaderNav/HeaderNav.js';
 import SideBar from './components/SideBar/SideBar';
 import Medicines from './components/Home/Home.jsx';
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+//Add Search Items
+//Deploy backend and call from there must be implelemented
+//Navigation must be made properly
 function App() {
   return (
     <div className="App">
