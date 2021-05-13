03e4b7ab7d4cd8acfe03ead752097046ee57ce87
Hari Prasad <hariprasad572415@gmail.com>
2021-05-13 13:17:05 +0530

    Removed name search repetition

--- src/services/fakeMedicineService.js
+++ src/services/fakeMedicineService.js
@@ -169,10 +169,6 @@ export function getMedicine(id) {
   return medicines.find((m) => m._id === id);
 }
 
-export function getMedicineByName(name) {
-  return medicines.find((m) => m.name.toLowerCase() === name);
-}
-
 export function deleteMedicines(id) {
   let medicineInDb = medicines.find((m) => m._id === id);
   medicines.splice(medicines.indexOf(medicineInDb), 1);
